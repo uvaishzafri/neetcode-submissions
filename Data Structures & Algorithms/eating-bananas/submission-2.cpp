ce75907ddcab119af05fc498c85b27d6bb67e137
Uvaish Zafri <35339873+uvaishzafri@users.noreply.github.com>
2026-08-13 11:13:26 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    bool isEatable(vector<int> piles, int h, int k) {
+        int timeTaken = 0;
+        for (auto it : piles) {
+            timeTaken+=it/k;
+            if(it%k) timeTaken+=1;
+        }
+        return timeTaken<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = INT_MAX;
+        int result = INT_MAX;
+
+        while (l <= r) {
+            int mid = l+ (r-l)/ 2;
+            if(isEatable(piles,h,mid)){
+                result = min(result,mid);
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+            
+        }
+        return result;
+    }
+};
