4d40f22873da4a35fc5ffa0aaa3abe045a6cd7a5
Uvaish Zafri <35339873+uvaishzafri@users.noreply.github.com>
2026-08-16 19:36:50 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+   public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](Interval a, Interval b) { return a.start < b.start; });
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (intervals[i - 1].end > intervals[i].start) return false;
+        }
+        return true;
+    }
+};
